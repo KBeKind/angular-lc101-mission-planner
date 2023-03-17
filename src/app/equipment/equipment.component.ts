044bce9871c959ca87e624a599ca2038ca8d4583
KBeKind <79383539+KBeKind@users.noreply.github.com>
2023-03-16 19:11:44 -0500

    finished first part of studio

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Equipment } from './equipment.interface';
+
 
 @Component({
   selector: 'app-equipment',
@@ -6,26 +8,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./equipment.component.css']
 })
 export class EquipmentComponent implements OnInit {
-   equipmentItems: object[] = [
-       {name: 'Duct Tape', mass: 0.5},
-       {name: 'Space Camera', mass: 20},
-       {name: 'Food', mass: 150},
-       {name: 'Oxygen Tanks', mass: 400},
-       {name: 'AE-35 Unit', mass: 5},
-       {name: 'ISS Supplies', mass: 800},
-       {name: 'Water', mass: 250},
-       {name: 'Satellite', mass: 1200},
-       {name: 'R2 Unit', mass: 32}
-   ];
-   cargoHold: object[] = [];
-   cargoMass: number = 0;
-   maximumAllowedMass: number = 2000;
-   maxItems: number = 10;
+  equipmentItems: object[] = [
+    { name: 'Duct Tape', mass: 0.5 },
+    { name: 'Space Camera', mass: 20 },
+    { name: 'Food', mass: 150 },
+    { name: 'Oxygen Tanks', mass: 400 },
+    { name: 'AE-35 Unit', mass: 5 },
+    { name: 'ISS Supplies', mass: 800 },
+    { name: 'Water', mass: 250 },
+    { name: 'Satellite', mass: 1200 },
+    { name: 'R2 Unit', mass: 32 }
+  ];
+  cargoHold: object[] = [];
+  cargoMass: number = 0;
+  maximumAllowedMass: number = 2000;
+  maxItems: number = 10;
+  nearMax: boolean = false;
 
-   constructor() { }
 
-   ngOnInit() { }
 
-   // Code your addItem function here:
-   
+  constructor() { }
+
+  ngOnInit() { }
+
+  // Code your addItem function here:
+  addItem(equipment: Equipment) {
+
+    this.cargoHold.push(equipment);
+    this.cargoMass += equipment.mass;
+    if (this.maximumAllowedMass - this.cargoMass <= 200) {
+      return true;
+    }
+    return false;
+
+  }
+
+  checkMass(equipment: Equipment) {
+    if (this.cargoHold.length === this.maxItems || this.cargoMass + equipment.mass > this.maximumAllowedMass) {
+      return true;
+    }
+    return false;
+  }
+
+  emptyHold() {
+    this.cargoHold = [];
+    this.cargoMass = 0;
+    this.nearMax = false;
+  }
 }
+
